2b27a8a1b543339ed5759428572e39069628bd40
Jerome Terry <jerome.terry@d2l.com>
2015-05-02 13:20:40 -0230

    Extracting apt-get commands to separate file
    Instead of including the apt-get commands directly in the Vagrantfile,
    include them in a configure.sh file. Makes the Vagrantfile easier to
    read

---
+++ SimpleVagrantExample/configure.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+sudo apt-get install -y build-essential
