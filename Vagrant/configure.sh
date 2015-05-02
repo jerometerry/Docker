a1f88551d9fd161868fdb985a5b1bf93c13a705a
Jerome Terry <jerome.terry@d2l.com>
2015-05-02 13:54:48 -0230

    Configuring the VM to use Xfce4

--- Vagrant/configure.sh
+++ Vagrant/configure.sh
@@ -1,3 +1,15 @@
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-sudo apt-get install -y build-essential
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y \
+	-o Dpkg::Options::="--force-confdef" \
+	-o Dpkg::Options::="--force-confold" \
+	upgrade
+
+sudo apt-get install -y \
+	build-essential \
+	virtualbox-guest-dkms \
+	virtualbox-guest-utils \
+	virtualbox-guest-x11\
+	xfce4
+
+sudo startxfce4&
+sudo VBoxClient-all
